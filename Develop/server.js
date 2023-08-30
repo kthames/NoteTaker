c3b814c8c8de2935ad1a28194fb5ae505c81b239
kthames <135296109+kthames@users.noreply.github.com>
2023-08-30 16:46:14 -0700

    Push to save progress on server.js

--- Develop/server.js
+++ Develop/server.js
@@ -11,13 +11,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
+app.use(express.static('public'));
+
 // GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET route for index page
-app.get('*', (req, res) =>
+app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
